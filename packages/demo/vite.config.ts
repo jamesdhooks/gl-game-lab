dfc5340b1fe00b2b56172506e8b5d8745ad9f1b1
jamesdhooks <jamesdhooks@gmail.com>
2026-07-11 17:29:33 -0400

    ci(demo): restore GitHub Pages deployment

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.GL_GAME_LAB_BASE_PATH ?? '/',
   plugins: [react()],
 });
